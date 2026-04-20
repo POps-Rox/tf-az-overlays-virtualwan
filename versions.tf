c2aecf6fec5d949ec0b2ca2b10fc04705a121088
Josh Luedeman <josh@joshluedeman.com>
2026-04-19 23:16:01 -0400

    feat: remove azurenoops provider dependency (#5)
    * feat: remove azurenoops provider dependency
    
    * fix: rename provider local name to popsrox and remove stale lock file

--- versions.tf
+++ versions.tf
@@ -8,9 +8,9 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.116"
     }
-    azurenoopsutils = {
-      source  = "azurenoops/azurenoopsutils"
-      version = "~> 1.0.4"
+    popsrox = {
+      source  = "POps-Rox/azutils"
+      version = "~> 1.0"
     }
   }
 }
